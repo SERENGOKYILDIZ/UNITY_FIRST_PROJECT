2daf4d0620a7b57cfdad54b4e62af056079440f5
Semi Eren Gökyıldız <gokyildizsemieren@gmail.com>
2025-07-18 00:16:15 +0300

    Learned Components Part 1

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public GameObject myLight;
+    public int sayi;
+
+    public Light light;
+    void Start()
+    {
+        //print(myLight);
+        myLight.GetComponent<Light>().intensity = 100;
+
+        /* 
+         Eğer "GetComponent<Light>().intensity = 100;" şeklinde yazsaydım.
+         Direk kodun bağlı olduğu GameObject'in içinde arayacaktı Component'i.
+        */
+
+        //print(this);
+
+        //print(sayi);
+
+
+        //-------------------------------------------------------------------//
+
+        // Görüldüğü gibi Component'lar bir değişken gibi tanımlanabilir ve kullanılabilir.
+        light = myLight.GetComponent<Light>();
+        light.intensity = 100;
+        light.color = Color.red;
+    }
+
+    public void Print()
+    {
+        print("Yazdırma işlemi");
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public Control control;
+    private void Awake()
+    {
+        //GetComponent<Control>().sayi = 200;
+
+        //GetComponent<Control>().Print();
+
+        // Görüldüğü gibi kendi yazdığımız scriptler bile değişken olarak tanımlanabilir.
+        control = GetComponent<Control>();
+        control.sayi = 1000;
+    }
+}
