83f8ee9639089ef11f4909c58b8b042a1bec8319
Semi Eren Gökyıldız <gokyildizsemieren@gmail.com>
2025-07-18 00:35:01 +0300

    Learned Components Part 2, Bounds and Vectors

---
+++ Assets/Scripts/CubeControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CubeControl : MonoBehaviour
+{
+    private void Start()
+    {
+        // Bounds = Box Collider'ın sınırlarının konum bilgisine erişim sağlar.
+        float max = GetComponent<BoxCollider>().bounds.max.y;
+        float min = GetComponent<BoxCollider>().bounds.min.y;
+        print("Max : " + max + ", Min : " + min);
+
+        //Vector
+        Vector2 vector;
+        vector.x = 3f;
+        vector.y = 4f;
+        print(vector);
+    }
+}
