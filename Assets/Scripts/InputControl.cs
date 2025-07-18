0e83cc99d41da5d694bf17f9b816cfc501671bba
Semi Eren Gökyıldız <gokyildizsemieren@gmail.com>
2025-07-18 19:05:30 +0300

    Learned Input.GetKey and Input.GetMouseBotton

---
+++ Assets/Scripts/InputControl.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class InputControl : MonoBehaviour
+{
+    private void Update()
+    {
+        // 0 = Sol tık, 1 = Sağ tık, 2 = Orta tuş
+
+        // GetMouseButtonDown = Bir kez basınca çalışır
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Sol Tık Tıklandı");
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            print("Sağ Tık Tıklandı");
+        }
+        else if (Input.GetMouseButtonDown(2))
+        {
+            print("Orta Tık Tıklandı");
+        }
+        */
+
+        // GetMouseButton = Basılı tutulduğu müddetçe çalışır
+        /*
+        if (Input.GetMouseButton(0))
+        {
+            print("Sol Tık Tıklanıyor");
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            print("Sağ Tık Tıklanıyor");
+        }
+        else if (Input.GetMouseButton(2))
+        {
+            print("Orta Tık Tıklanıyor");
+        }
+        */
+
+        // GetMouseButtonUp = Basıp çekince bir kez çalışır
+        /*
+        if (Input.GetMouseButtonUp(0))
+        {
+            print("Sol Tık Tıklanmış");
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            print("Sağ Tık Tıklanmış");
+        }
+        else if (Input.GetMouseButtonUp(2))
+        {
+            print("Orta Tık Tıklanmış");
+        }
+        */
+
+        // GetMouseButton ve türevleri nasılsa aynısı GetKey ve türevleri içinde geçerli
+        /*
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            print("F tuşuna Tıklandı");
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            print("F tuşuna Tıklanıyor");
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            print("F tuşuna Tıklanmış");
+        }
+        */
+    }
+}
