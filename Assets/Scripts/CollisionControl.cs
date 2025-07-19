785c6c54a3c591309c28cfbf6ccf7edddcfe0aac
Semi Eren Gökyıldız <gokyildizsemieren@gmail.com>
2025-07-19 14:57:30 +0300

    Learned onTrigger and onCollision

---
+++ Assets/Scripts/CollisionControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CollisionControl : MonoBehaviour
+{
+    // Eğer herhangi bir collider componentli bir GameObject ile çarpışırsa bu fonksiyon çalışır.
+    // İki cismin çarpışmasının tetiklenmesinde kullanılabilir.
+    // Not: İki GameObject'ten en az herhangi birinde RigidBody componenti olmalı.
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Eleman: " + collision.gameObject.name);
+    }
+    */
+
+    // Bu ise bizim collider'ımızın içinden geçmesi durumunda çalışır.
+    // Oyuncunun bir odaya girdiği zaman tetik alması gibi durumlarda kullanılabilir.
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.name);
+    }
+}
