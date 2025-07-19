9f1d9bfb0bfc31912b8bf9f64082536d2e8cd7be
Semi Eren Gökyıldız <gokyildizsemieren@gmail.com>
2025-07-19 16:30:47 +0300

    Learned Player and Camera Controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensitiviy = 100f;
+    public GameObject player;
+    float xRotation;
+
+    private void Start()
+    {
+        // Mouse odağını oyuna odaklar.
+        Cursor.lockState = CursorLockMode.Locked;
+        // Mouse imlecini gizler
+        Cursor.visible = false;
+    }
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitiviy;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitiviy;
+
+        xRotation -= mouseY;                                            // Ters olduğu için çıkarttık
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);                  // Sınır belirlemek için kullanılır.
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);    // X eksenini bu şekilde snırlı atadık.
+
+        player.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //---------- Player Movement ----------//
+    public float speed;
+
+    //---------- FlashLight ----------//
+    public GameObject flashLight;
+    bool is_on;
+
+    //---------- Fire ----------//
+    RaycastHit hit;
+
+    private void Start()
+    {
+        //---------- FlashLight ----------//
+        is_on = flashLight.GetComponent<Light>().enabled;
+    }
+    private void Update()
+    {
+        //---------- Player Movement ----------//
+        // Tuşların oluşturduğu bir vektörü değişkene atadık.
+        Vector3 playerVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // Hız için ayrı bir vektör tanımladık.
+        Vector3 moveVelocity = playerVector * Time.deltaTime * speed;
+        // Hız ile konum değişimi yapılıyor. (Güncellendi kamera için)
+        transform.Translate(moveVelocity);
+
+        //---------- FlashLight ----------//
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            is_on = !is_on;
+            flashLight.GetComponent<Light>().enabled = is_on;
+        }
+
+        /*
+         * GameObject.FindGameObjectWithTag("MainCamera") = Camera.main;
+         * 
+         * aynı anlama gelir. (Özel durum)
+         */
+
+        //---------- Fire ----------//
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+            {
+                if(hit.collider.gameObject.tag == "Enemy")
+                {
+                    // Belirtilen GameObject'i siler
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
