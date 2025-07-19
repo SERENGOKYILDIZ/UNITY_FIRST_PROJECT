b4611cbb899035f9c50989b0ba2032a7979dd1a9
Semi Eren Gökyıldız <gokyildizsemieren@gmail.com>
2025-07-19 18:05:27 +0300

    Made player and camera system for new input system

---
+++ Assets/Scripts/CameraController_New_Input_System.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController_New_Input_System : MonoBehaviour
+{
+    public float mouseSensitiviy = 100f;
+    public GameObject player;
+    float xRotation;
+    public InputActionReference mouse;
+
+    private void Start()
+    {
+        // Mouse odağını oyuna odaklar.
+        Cursor.lockState = CursorLockMode.Locked;
+        // Mouse imlecini gizler
+        Cursor.visible = false;
+    }
+    private void Update()
+    {
+        Vector2 delta = Mouse.current.delta.ReadValue();
+
+        float mouseX = delta.x * Time.deltaTime * mouseSensitiviy / 20;
+        float mouseY = delta.y * Time.deltaTime * mouseSensitiviy / 20;
+
+        xRotation -= mouseY;                                            // Ters olduğu için çıkarttık
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);                  // Sınır belirlemek için kullanılır.
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);    // X eksenini bu şekilde snırlı atadık.
+
+        player.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerController_New_Input_System.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController_New_Input_System : MonoBehaviour
+{
+    //---------- Player Movement ----------//
+    public float speed;
+    public InputActionReference move;
+
+    //---------- FlashLight ----------//
+    public GameObject flashLight;
+    public InputActionReference flash;
+    bool is_on;
+
+    //---------- Fire ----------//
+    RaycastHit hit;
+    public InputActionReference fire;
+    private void Start()
+    {
+        //---------- FlashLight ----------//
+        is_on = flashLight.GetComponent<Light>().enabled;
+    }
+    private void Update()
+    {
+        //---------- Player Movement ----------//
+        // Tuşların oluşturduğu bir vektörü değişkene atadık.
+        Vector2 vec = move.action.ReadValue<Vector2>();
+        Vector3 playerVector = new Vector3(vec.x, 0, vec.y);
+        // Hız için ayrı bir vektör tanımladık.
+        Vector3 moveVelocity = playerVector * Time.deltaTime * speed;
+        // Hız ile konum değişimi yapılıyor. (Güncellendi kamera için)
+        transform.Translate(moveVelocity);
+
+    }
+    private void OnEnable()
+    {
+        flash.action.Enable();
+        flash.action.started += Flash;
+
+        fire.action.Enable();
+        fire.action.started += Fire;
+    }
+
+    private void OnDisable()
+    {
+        flash.action.started -= Flash;
+        fire.action.started -= Fire;
+    }
+
+    //---------- FlashLight ----------//
+    private void Flash(InputAction.CallbackContext context)
+    {
+        is_on = !is_on;
+        flashLight.GetComponent<Light>().enabled = is_on;
+
+    }
+    //---------- Fire ----------//
+    private void Fire(InputAction.CallbackContext context)
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject.tag == "Enemy")
+            {
+                // Belirtilen GameObject'i siler
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
